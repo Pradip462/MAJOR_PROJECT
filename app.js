af0653bc2ae2f05907e1b8b1d76145ff9590cd69
Pradip462 <pradipofficial462@gmail.com>
2024-10-03 20:31:31 +0530

    Update the sliding filter part

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ async function main() {
   await mongoose.connect(dbUrl);
 }
 
+app.use(express.static('public'));
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended : true}));
